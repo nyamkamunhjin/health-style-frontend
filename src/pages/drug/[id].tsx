a22cc451c2fdd192d56dc6d2cd001c270218e702
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2021-06-19 19:51:55 +0800

    header update

--- src/pages/drug/[id].tsx
+++ src/pages/drug/[id].tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable react/no-danger */
 /* eslint-disable react/no-unescaped-entities */
 import { useRequest } from 'ahooks';
@@ -11,7 +14,7 @@ import 'react-medium-image-zoom/dist/styles.css';
 
 const URL_IMAGE_PREFIX = 'http://103.50.204.55:9001/licemed-images/';
 const backgroundImage =
-  'https://c.pxhere.com/photos/65/f4/headache_pain_pills_medication_tablets_drugs_drugstore_medicine-565804.jpg!d';
+  'https://images.unsplash.com/photo-1584362917165-526a968579e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1982&q=80';
 const Card: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -24,14 +27,32 @@ const Card: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>WTF</title>
-      </Head>
+      {drug.data && (
+        <Head>
+          <title>{drug.data.name}</title>
+          <meta name="title" content={drug.data.name} />
+          <meta
+            name="image"
+            content={drug.data?.licemed_item_dtl?.images[0]?.path || ''}
+          />
+          <meta name="description" content={drug.data?.inter_name} />
+          <meta name="author" content={drug.data.supplier} />
+          <meta name="og:title" content={drug.data.name} />
+          <meta
+            name="og:image"
+            content={drug.data?.licemed_item_dtl?.images[0]?.path || ''}
+          />
+          <meta name="og:description" content={drug.data?.inter_name} />
+          <meta name="og:author" content={drug.data.supplier} />
+        </Head>
+      )}
       <div
         className="w-screen h-screen bg-cover bg-scroll"
-        style={{ background: `url(${backgroundImage})` }}
+        style={{
+          background: `url(${backgroundImage})`,
+        }}
       >
-        <div className="glass mx-auto max-w-4xl w-full pt-20 flex flex-col gap-4 p-4">
+        <div className="glass mx-auto max-w-4xl w-full pt-10 flex flex-col gap-4 p-4">
           <button
             className="btn w-24"
             type="button"
@@ -39,47 +60,62 @@ const Card: NextPage = () => {
           >
             Буцах
           </button>
-          <p className="text-4xl font-semibold">{drug.data?.name}</p>
-          <div className="flex flex-col">
-            <span className="text-gray-600">
-              Тун: <span className="text-black">{drug.data?.med_dose}</span>
-            </span>
-            <span className="text-gray-600">
-              Барааны дугаар:{' '}
-              <span className="text-black">{drug.data?.product_number}</span>
-            </span>
-            <span className="text-gray-600">
-              Нэршил:{' '}
-              <span className="text-black">{drug.data?.inter_name}</span>
-            </span>
-            <span className="text-gray-600">
-              Тодорхойлолт:{' '}
-              <span className="text-black">{drug.data?.specification}</span>
-            </span>
-            <span className="text-gray-600">
-              Тун: <span className="text-black">{drug.data?.med_dose}</span>
-            </span>
-            <span className="text-gray-600">
-              Савалгаа:{' '}
-              <span className="text-black">{drug.data?.packaging}</span>
-            </span>
-            <span className="text-gray-600">
-              Улс: <span className="text-black">{drug.data?.country}</span>
-            </span>
-            <span className="text-gray-600">
-              Нийлүүлэгч:{' '}
-              <span className="text-black">{drug.data?.supplier}</span>
-            </span>
-            <span className="text-gray-600">
-              Баркод: <span className="text-black">{drug.data?.barcode}</span>
-            </span>
+          <div className="flex gap-2">
+            {drug.data?.licemed_item_dtl?.images[0]?.path && (
+              <img
+                className="w-72 rounded-xl object-contain border border-solid border-black"
+                src={`${URL_IMAGE_PREFIX}${drug.data?.licemed_item_dtl?.images[0].path}`}
+                alt="Эмний зураг"
+              />
+            )}
+            <div>
+              <p className="text-4xl font-semibold">{drug.data?.name}</p>
+              <div className="flex flex-col">
+                <span className="text-gray-600">
+                  Тун: <span className="text-black">{drug.data?.med_dose}</span>
+                </span>
+                <span className="text-gray-600">
+                  Барааны дугаар:{' '}
+                  <span className="text-black">
+                    {drug.data?.product_number}
+                  </span>
+                </span>
+                <span className="text-gray-600">
+                  Нэршил:{' '}
+                  <span className="text-black">{drug.data?.inter_name}</span>
+                </span>
+                <span className="text-gray-600">
+                  Тодорхойлолт:{' '}
+                  <span className="text-black">{drug.data?.specification}</span>
+                </span>
+                <span className="text-gray-600">
+                  Тун: <span className="text-black">{drug.data?.med_dose}</span>
+                </span>
+                <span className="text-gray-600">
+                  Савалгаа:{' '}
+                  <span className="text-black">{drug.data?.packaging}</span>
+                </span>
+                <span className="text-gray-600">
+                  Улс: <span className="text-black">{drug.data?.country}</span>
+                </span>
+                <span className="text-gray-600">
+                  Нийлүүлэгч:{' '}
+                  <span className="text-black">{drug.data?.supplier}</span>
+                </span>
+                <span className="text-gray-600">
+                  Баркод:{' '}
+                  <span className="text-black">{drug.data?.barcode}</span>
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="w-full flex flex-wrap justify-center gap-2 items-start">
+          <p className="text-4xl font-semibold">Зурагнууд</p>
+          <div className="w-full flex flex-wrap gap-2 items-start">
             {drug.data?.licemed_item_dtl?.images.map(
               (each: { path: string }) => (
                 <Zoom>
                   <img
-                    className="w-72 object-contain border border-solid border-black"
+                    className="w-72 rounded-xl object-contain border border-solid border-black"
                     src={`${URL_IMAGE_PREFIX}/${each.path}`}
                     alt="Эмний зураг"
                   />
@@ -88,8 +124,8 @@ const Card: NextPage = () => {
             )}
           </div>
           {drug.data?.licemed_item_dtl?.content !== '<span></span>' && (
-            <div>
-              <p className="">Хэрэглэх заавар</p>
+            <div className="flex flex-col gap-4">
+              <p className="text-4xl font-semibold">Хэрэглэх заавар</p>
               <div
                 dangerouslySetInnerHTML={{
                   __html: drug.data?.licemed_item_dtl?.content || '',
